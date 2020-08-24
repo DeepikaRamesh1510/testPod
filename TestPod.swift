345df4ecc83d61be65cb1f1f603fe46635963360
Deepika <deepika.ram@anywhere.co>
2020-08-24 10:49:07 +0530

    Initial commit

---
+++ TestPod.swift
@@ -0,0 +1,12 @@
+//
+//  TestPod.swift
+//  FBSnapshotTestCase
+//
+//  Created by Deepika on 24/08/20.
+//
+
+import Foundation
+
+public func getFullName(firstName: String, lastName: String) -> String {
+    return firstName + "" + lastName
+}
